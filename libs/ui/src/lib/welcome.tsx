499f4384f21a0d5f5f22cbf4a253e89e92b60dd8
Roger Urich <rogerdurich@gmail.com>
2025-05-20 16:09:31 -0500

    some basic visuals with components and data being utilized

--- libs/ui/src/lib/welcome.tsx
+++ libs/ui/src/lib/welcome.tsx
@@ -1,11 +1,14 @@
 import { utils } from '@a1c/utils';
 
 export function Welcome() {
-
   return (
-    <div>
-      <p>a package imported from UI!</p>
-      <p>We are using {utils()}</p>
+    <div className="p-4">
+      <h1 className="text-4xl font-medium text-gray-800 mb-4">Hello there,</h1>
+      <p className="text-2xl text-gray-700">
+        Here we can use a package imported from UI!
+        <br />
+        We are using {utils()}
+      </p>
     </div>
   );
 }
