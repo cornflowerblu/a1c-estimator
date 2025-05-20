f07d218e80e285ef30ee102da79d92c1a70b9273
Roger Urich <rogerdurich@gmail.com>
2025-05-20 17:10:30 -0500

    fix async test

--- libs/ui/src/lib/welcome.spec.tsx
+++ libs/ui/src/lib/welcome.spec.tsx
@@ -1,9 +1,22 @@
 import { render } from '@testing-library/react';
-import {Welcome} from './welcome';
+import { Welcome } from './welcome';
+
+// Mock the imported modules
+jest.mock('@a1c/utils', () => ({
+  utils: () => 'mocked utils',
+}));
+
+jest.mock('@a1c/data', () => ({
+  dataAccess: () => 'mocked data access',
+}));
 
 describe('welcome', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<Welcome />);
+  it('should render successfully', async () => {
+    // Since Welcome is now an async component, we need to await it
+    const WelcomeComponent = await Welcome();
+    
+    // Render the result of the async component
+    const { baseElement } = render(WelcomeComponent);
     expect(baseElement).toBeTruthy();
   });
 });
