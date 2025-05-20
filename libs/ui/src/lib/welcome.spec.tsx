433277be1749b627d33c9a0bf1424e46b66bed0d
Roger Urich <rogerdurich@gmail.com>
2025-05-20 15:20:19 -0500

    setting up nx and praying it works

---
+++ libs/ui/src/lib/welcome.spec.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+import Welcome from './welcome';
+
+describe('welcome', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<Welcome />);
+    expect(baseElement).toBeTruthy();
+  });
+});
